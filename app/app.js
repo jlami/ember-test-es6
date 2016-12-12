25fc4a8685fd8ec0fb165a0b20a14e44f7d10045
Joren l'Ami <joren@distinct.nl>
2016-12-12 22:20:36 +0100

    Resolve Template via app

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-rollup-initializers';
 import config from './config/environment';
 import router from './router';
+import appTemplate from './templates/application';
 
 let App;
 
@@ -13,6 +14,7 @@ App = Ember.Application.extend({
   podModulePrefix: config.podModulePrefix,
   Resolver,
   MainRouter: router,
+  ApplicationTemplate: appTemplate,
 });
 
 loadInitializers(App, config.modulePrefix);
